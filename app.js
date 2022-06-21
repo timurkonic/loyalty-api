4ddffc990a95f124d6a5765993933a3376a866a4
Timur Konic <timur.konic@gmail.com>
2022-06-21 09:49:15 +0300

    Get account endpoint

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
-
 const mysql = require('mysql2');
 
 const dbhost = process.env.DBHOST || 'localhost';
@@ -18,15 +17,39 @@ const pool = mysql.createPool({
     user: dbuser,
     password: dbpass,
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: 20,
     queueLimit: 0
 });
 
+const processError = (err, req, res, next) => {
+    if (err)
+        return res.json({error: err.message || 'Unknown error'});
+};
+
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/v1/', router);
+app.use(processError);
 
-router.get('/', (req, res) => {
-    res.send("Hello world");
+router.get('/account/:id', (req, res, next) => {
+    const id = req.params.id;
+    const result = pool.query(`
+        select a.id, a.balance, a.balance_bns, a.type, at.name as type_name, a.active, a.block, bt.name as block_name,
+            a.owner_family_name, a.owner_first_name, a.owner_third_name, date_format(a.owner_birthday, '%d/%m/%Y') as owner_birthday, a.owner_phone, a.owner_email, a.discount
+        from account a
+        left join account_type at on at.id = a.type
+        left join block_type bt on bt.id = a.block
+        where a.id = ?`, [id], (err, rows) => {
+        if (err) {
+            console.log(err);
+            return next({ error: 'Internal error' });
+        }
+
+        if (rows.length === 0)
+            return res.json({ error: 'Account not found' });
+
+        return res.json(rows[0]);
+    });
 });
 
 const port = process.env.PORT || 8080;
