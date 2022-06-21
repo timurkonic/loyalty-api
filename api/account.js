04256457c3e7aa6815db077c16d1cebcdadf9a25
Timur Konic <timur.konic@gmail.com>
2022-06-21 15:10:45 +0300

    Some input value tests

--- api/account.js
+++ api/account.js
@@ -2,7 +2,7 @@ import pool from '../db/pool.js';
 
 const get = (req, res, next) => {
     const id = req.params.id;
-    const result = pool.query(`
+    pool.query(`
         select a.id, a.balance, a.balance_bns, a.type, at.name as type_name, a.active, a.block, bt.name as block_name,
             a.owner_family_name, a.owner_first_name, a.owner_third_name, date_format(a.owner_birthday, '%d/%m/%Y') as owner_birthday, a.owner_phone, a.owner_email, a.discount,
             if (a.wpass is null, a.wtmpass, null) as wtmpass
@@ -16,7 +16,7 @@ const get = (req, res, next) => {
         }
 
         if (rows.length === 0)
-            return res.json({ error: 'Account not found' });
+            return res.json({ error: 'Карта не найдена' });
 
         return res.json(rows[0]);
     });
--- api/transaction.js
+++ api/transaction.js
@@ -12,12 +12,33 @@ const post = (req, res, next) => {
             });
             break;
         default:
-            return res.json({error: "Unknown transaction type"});
+            return res.json({error: 'Неизвестный тип транзакции'});
     }
 }
 
 const paybonus = (account, amount, cassa, chek_sn, cb) => {
-    cb("Insufficient balance", null);
+    if (amount <= 0)
+        return cb('Неверная сумма списания', null);
+
+    pool.query('select balance_bns, active, block, owner_filled from account where id = ?', [account], (err, rows) => {
+        if (err) {
+            console.log(err);
+            return cb('Internal error', null);
+        }
+
+        if (rows.length === 0)
+            return cb('Карта не найдена', null);
+
+        if (rows[0].active === 0 || rows[0].block !== 0)
+            return cb('Карта заблокирована', null);
+
+        if (rows[0].balance_bns < amount)
+            return cb('Недосточный баланс', null);
+
+        const new_balance = Math.round(rows[0].balance_bns - amount, 2);
+
+        return cb(false, {new_balance: new_balance});
+    });
 }
 
 
