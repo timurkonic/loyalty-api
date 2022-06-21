c30c851196974e78d563b067761abf630270580a
Timur Konic <timur.konic@gmail.com>
2022-06-21 13:28:45 +0300

    Add wtmpass field

--- api/account.js
+++ api/account.js
@@ -4,7 +4,8 @@ const get = (req, res, next) => {
     const id = req.params.id;
     const result = pool.query(`
         select a.id, a.balance, a.balance_bns, a.type, at.name as type_name, a.active, a.block, bt.name as block_name,
-            a.owner_family_name, a.owner_first_name, a.owner_third_name, date_format(a.owner_birthday, '%d/%m/%Y') as owner_birthday, a.owner_phone, a.owner_email, a.discount
+            a.owner_family_name, a.owner_first_name, a.owner_third_name, date_format(a.owner_birthday, '%d/%m/%Y') as owner_birthday, a.owner_phone, a.owner_email, a.discount,
+            if (a.wpass is null, a.wtmpass, null) as wtmpass
         from account a
         left join account_type at on at.id = a.type
         left join block_type bt on bt.id = a.block
