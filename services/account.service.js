0d4d379f0bec2e2710d15a6c924851dd00f30946
Timur Konic <timur.konic@gmail.com>
2022-06-27 14:49:47 +0300

    Add birthday action field

--- services/account.service.js
+++ services/account.service.js
@@ -3,7 +3,7 @@ import pool from '../db/pool.js';
 class AccountService {
 
     async getAccount(id) {
-        const select = await pool.query(`
+        const select_account = await pool.query(`
             select a.id, a.balance, a.balance_bns, a.type, at.name as type_name, a.active, a.block, bt.name as block_name,
                 a.owner_family_name, a.owner_first_name, a.owner_third_name, date_format(a.owner_birthday, '%d/%m/%Y') as owner_birthday, a.owner_phone, a.owner_email, a.owner_filled, a.discount,
                 if (a.wpass is null, a.wtmpass, null) as wtmpass
@@ -11,10 +11,15 @@ class AccountService {
             left join account_type at on at.id = a.type
             left join block_type bt on bt.id = a.block
             where a.id = ?`, [id]);
-        if (select[0].length === 0)
+        if (select_account[0].length === 0)
             return false;
         
-        return select[0][0];
+        let account = select_account[0][0];
+
+        const select_birthday = await pool.query('select account from action_birthday where start_dt <= curdate() and end_dt >= curdate() and account = ?', [id]);
+        account['birthday_action'] = select_birthday[0].length > 0;
+
+        return account;
     }
 
 }
