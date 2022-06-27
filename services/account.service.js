a28d78921658ab1e6ddb6228c6145f3e2d63ff80
Timur Konic <timur.konic@gmail.com>
2022-06-27 11:55:35 +0300

    Add owner filled field

--- services/account.service.js
+++ services/account.service.js
@@ -5,7 +5,7 @@ class AccountService {
     async getAccount(id) {
         const select = await pool.query(`
             select a.id, a.balance, a.balance_bns, a.type, at.name as type_name, a.active, a.block, bt.name as block_name,
-                a.owner_family_name, a.owner_first_name, a.owner_third_name, date_format(a.owner_birthday, '%d/%m/%Y') as owner_birthday, a.owner_phone, a.owner_email, a.discount,
+                a.owner_family_name, a.owner_first_name, a.owner_third_name, date_format(a.owner_birthday, '%d/%m/%Y') as owner_birthday, a.owner_phone, a.owner_email, a.owner_filled, a.discount,
                 if (a.wpass is null, a.wtmpass, null) as wtmpass
             from account a
             left join account_type at on at.id = a.type
